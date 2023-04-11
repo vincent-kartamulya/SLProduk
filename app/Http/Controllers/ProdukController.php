23e3b1a3640bf9b085fe4783b6419bdd5776c597
vincent-kartamulya <vincent.learn01@gmail.com>
2023-04-11 11:31:12 +0700

    ui

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -10,6 +10,7 @@ class ProdukController extends Controller
         $produk = [
             ['id' => '1',
             'name' => 'Indomie Goreng Spesial',
+            'image' => 'indomiegoreng.jpg',
             'status' => 'R',
             'price' => 'Rp. 3000',
             'desc' => 'Produk terpopuler dari brand Indomie, Indomie Goreng pertama diluncurkan pada tahun 1982 dan telah merambah banyak negara termasuk Amerika Serikat, Australia, Inggris, Timur Tengah dan China. Jangan lewatkan berbagai pilihan rasa yang ditawarkan, Indomie Goreng memang yang paling komplit!'
@@ -17,13 +18,15 @@ class ProdukController extends Controller
 
             ['id' => '2',
             'name' => 'Indomie Rasa Kari Ayam',
+            'image' => 'indomiekari.jpg',
             'status' => 'R',
             'price' => 'Rp. 5000',
             'desc' => 'Indomie kuah merupakan salah satu varian yang rasanya khusus diambil dari bumbu-bumbu makanan khas Indonesia. Perpaduan antara mi, kuah dan bumbu otentik makanan khas Indonesia menjadikan Indomie kuah makanan yang sangat spesial apalagi bila disajikan dikala cuaca dingin atau hujan.'
             ],
 
             ['id' => '3',
-            'name' => 'Indomie Rasa Mi Kocok Bandung',
+            'name' => 'Indomie Mi Kocok Bandung',
+            'image' => 'indomiemiekocok.jpg',
             'status' => 'SSR',
             'price' => 'Rp. 8900',
             'desc' => 'Indomie selalu berhasil memikat penggemar mi dengan cita rasa khas. Kali ini, mereka membuat varian rasa mi kocok, khas Kota Bandung. Bukan rasanya saja yang mirip, tapi tekstur minya juga dibuat serupa. Apalagi Indomie melengkapi rasa mi kocok Bandung dengan topping bakso, membuatnya tambah nikmat dan lezat'
@@ -31,6 +34,7 @@ class ProdukController extends Controller
 
             ['id' => '4',
             'name' => 'Indomie Rasa Baso Sapi',
+            'image' => 'indomiebaso.png',
             'status' => 'SR',
             'price' => 'Rp. 4000',
             'desc' => 'Indomie kuah merupakan salah satu varian yang rasanya khusus diambil dari bumbu-bumbu makanan khas Indonesia. Perpaduan antara mi, kuah dan bumbu otentik makanan khas Indonesia menjadikan Indomie kuah makanan yang sangat spesial apalagi bila disajikan dikala cuaca dingin atau hujan.'
@@ -38,13 +42,15 @@ class ProdukController extends Controller
 
             ['id' => '5',
             'name' => 'Indomie Mie Goreng Ayam Pop',
+            'image' => 'indomieayampop.webp',
             'status' => 'SR',
             'price' => 'Rp. 6900',
             'desc' => 'Rekomendasi mie kelima yang viral untukmu Indomie Mie Goreng Ayam Pop. Hampir sama dengan rendang, ayam pop ini juga banyak membuat pecinta Indomie penasaran dengan rasanya. Sehingga sekalinya keluar langsung laris dan berhasil menggoyang lidah para pecinta mienya. '
             ],
 
             ['id' => '6',
-            'name' => 'Indomie Seblak Hot Jeletot Mie Instan',
+            'name' => 'Indomie Seblak Hot Jeletot',
+            'image' => 'indomieseblak.jpg',
             'status' => 'SSR',
             'price' => 'Rp. 7800',
             'desc' => 'Orang Indonesia memang suka banget dengan makanan pedas sehingga hadirnya Indomie Seblak Hot Jeletot Mie Instan menjadi menu favorit mie bagi penyuka makanan pedas. Mie ini pedasnya pas, gurihnya dapet dan enak banget untuk dimakan saat hujan sebagai seblak instan tanpa perlu membuatnya ribet. '
@@ -56,6 +62,7 @@ class ProdukController extends Controller
             $produk = [
                 ['id' => '1',
                 'name' => 'Indomie Goreng Spesial',
+                'image' => 'indomiegoreng.jpg',
                 'status' => 'R',
                 'price' => 'Rp. 3000',
                 'desc' => 'Produk terpopuler dari brand Indomie, Indomie Goreng pertama diluncurkan pada tahun 1982 dan telah merambah banyak negara termasuk Amerika Serikat, Australia, Inggris, Timur Tengah dan China. Jangan lewatkan berbagai pilihan rasa yang ditawarkan, Indomie Goreng memang yang paling komplit!'
@@ -63,13 +70,15 @@ class ProdukController extends Controller
 
                 ['id' => '2',
                 'name' => 'Indomie Rasa Kari Ayam',
+                'image' => 'indomiekari.jpg',
                 'status' => 'R',
                 'price' => 'Rp. 5000',
                 'desc' => 'Indomie kuah merupakan salah satu varian yang rasanya khusus diambil dari bumbu-bumbu makanan khas Indonesia. Perpaduan antara mi, kuah dan bumbu otentik makanan khas Indonesia menjadikan Indomie kuah makanan yang sangat spesial apalagi bila disajikan dikala cuaca dingin atau hujan.'
                 ],
 
                 ['id' => '3',
-                'name' => 'Indomie Rasa Mi Kocok Bandung',
+                'name' => 'Indomie Mi Kocok Bandung',
+                'image' => 'indomiemiekocok.jpg',
                 'status' => 'SSR',
                 'price' => 'Rp. 8900',
                 'desc' => 'Indomie selalu berhasil memikat penggemar mi dengan cita rasa khas. Kali ini, mereka membuat varian rasa mi kocok, khas Kota Bandung. Bukan rasanya saja yang mirip, tapi tekstur minya juga dibuat serupa. Apalagi Indomie melengkapi rasa mi kocok Bandung dengan topping bakso, membuatnya tambah nikmat dan lezat'
@@ -77,6 +86,7 @@ class ProdukController extends Controller
 
                 ['id' => '4',
                 'name' => 'Indomie Rasa Baso Sapi',
+                'image' => 'indomiebaso.png',
                 'status' => 'SR',
                 'price' => 'Rp. 4000',
                 'desc' => 'Indomie kuah merupakan salah satu varian yang rasanya khusus diambil dari bumbu-bumbu makanan khas Indonesia. Perpaduan antara mi, kuah dan bumbu otentik makanan khas Indonesia menjadikan Indomie kuah makanan yang sangat spesial apalagi bila disajikan dikala cuaca dingin atau hujan.'
@@ -84,13 +94,15 @@ class ProdukController extends Controller
 
                 ['id' => '5',
                 'name' => 'Indomie Mie Goreng Ayam Pop',
+                'image' => 'indomieayampop.webp',
                 'status' => 'SR',
                 'price' => 'Rp. 6900',
                 'desc' => 'Rekomendasi mie kelima yang viral untukmu Indomie Mie Goreng Ayam Pop. Hampir sama dengan rendang, ayam pop ini juga banyak membuat pecinta Indomie penasaran dengan rasanya. Sehingga sekalinya keluar langsung laris dan berhasil menggoyang lidah para pecinta mienya. '
                 ],
 
                 ['id' => '6',
-                'name' => 'Indomie Seblak Hot Jeletot Mie Instan',
+                'name' => 'Indomie Seblak Hot Jeletot',
+                'image' => 'indomieseblak.jpg',
                 'status' => 'SSR',
                 'price' => 'Rp. 7800',
                 'desc' => 'Orang Indonesia memang suka banget dengan makanan pedas sehingga hadirnya Indomie Seblak Hot Jeletot Mie Instan menjadi menu favorit mie bagi penyuka makanan pedas. Mie ini pedasnya pas, gurihnya dapet dan enak banget untuk dimakan saat hujan sebagai seblak instan tanpa perlu membuatnya ribet. '
--- resources/views/produk.blade.php
+++ resources/views/produk.blade.php
@@ -5,16 +5,48 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="{{ asset('style.css') }}">
     <title>Document</title>
 </head>
-<body>
-    @foreach ($produk as $item)
-    <a href="/produk/{{$item['id']}}">
+<body class="body-custom">
+        <div class="text-center container py-5">
+            <h1 class="mt-4 mb-5 best-custom">Best Sellers</h1>
+
+            <div class="row">
+                @foreach ($produk as $item)
+                <div class="col-lg-4 col-md-12 mb-4">
+                    <div class="card
+                    @if ($item['status'] == 'R')
+                        silver
+                    @elseif ($item['status'] == 'SR')
+                        gold
+                    @else
+                        red
+                    @endif" >
+                        <div class="bg-image hover-zoom ripple ripple-surface ripple-surface-light"
+                        data-mdb-ripple-color="light">
+                            <img src={{$item['image']}} class="w-100" />
+                            <a href="#!">
+                                <div class="hover-overlay">
+                                    <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="card-body">
+                            <a href="" class="text-reset stretched-link">
+                                <h5 class="card-title mb-3 font-weight-bold">{{ $item['name'] }}</h5>
+                            </a>
+                            <p>Status : {{ $item['status'] }}</p>
+                            <h6 class="mb-3">{{ $item['price'] }}</h6>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+        </div>
+    {{-- <a href="/produk/{{$item['id']}}">
         <div class="container d-flex justify-content-around card-custom">
             <div class="card red">
-                {{-- <img class="card-img-top" src="{{ asset('images/' . $item['image']) }}"
-                    alt="{{ $item['name'] }}"> --}}
                 <div class="card-body
                 @if ($item['status'] == 'R')
                     silver
@@ -28,8 +60,7 @@
                 </div>
             </div>
         </div>
-    </a>
-    @endforeach
+    </a> --}}
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
